bd2c075c54300cdbcdcee6d79d4d9b9ac09ef87d
Chris Fritz <chrisvfritz@gmail.com>
2015-07-28 16:54:34 -0400

    started removing some of the frills

--- source/javascripts/vendor/creative.js
+++ source/javascripts/vendor/creative.js
@@ -8,32 +8,32 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    // $('a.page-scroll').bind('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    //     }, 1250, 'easeInOutExpo');
+    //     event.preventDefault();
+    // });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 51
+    // })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+    // $('.navbar-collapse ul li a').click(function() {
+    //     $('.navbar-toggle:visible').click();
+    // });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+    // $("h1").fitText(
+    //     1.2, {
+    //         minFontSize: '35px',
+    //         maxFontSize: '65px'
+    //     }
+    // );
 
     // Offset for Main Navigation
     $('#mainNav').affix({
