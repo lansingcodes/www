2e4bd0f84de0f97abc844a468529c93caa4ea5e1
Chris Fritz <chrisvfritz@gmail.com>
2015-07-31 03:38:23 -0400

    moved arch version of site to project root

--- source/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/jquery
-//= require vendor/bootstrap
-//= require vendor/jquery.easing
-//= require vendor/jquery.fittext
-//= require vendor/wow
-//= require vendor/creative
-//= require react
-//
-//= require lib/upcoming-events
