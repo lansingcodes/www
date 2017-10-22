17ac224324769dbc4a50c4b69eb7a1431d71e9c8
Joe Ryan <joe@clanryan.us>
2017-10-22 10:52:11 -0400

    Add FreeCodeCamp to beginners resources

--- src/config/beginner-resources.js
+++ src/config/beginner-resources.js
@@ -5,5 +5,15 @@ export default [
     desc: `
       <p>If you're a hard worker, ready to change careers, apply to <a href="https://codersfarm.com/training/">Coders Farm</a>. Spots are very limited, but should you be accepted, you'll receive free training in web development <em>and</em> a part-time job until placed in a full-time position with another company.</p>
     `
+  },
+  {
+    title: 'Free Code Camp',
+    url: 'https://www.freecodecamp.org',
+    desc: `
+      <p>A community that works to help people learn to code and gain 
+      experience contributing to open source projects used by non-profits. 
+      Learn to code by completing coding challenges and building projects. 
+      FreeCodeCamp is a donor supported non-profit and every aspect is 100% free.<p>
+    `
   }
 ]
