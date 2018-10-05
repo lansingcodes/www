d8346b65e6f1de48ee3983d0b2453181bced2d13
Davis <d.crain@kunzleigh.com>
2018-10-05 09:26:43 -0400

    display sponsor logos

--- src/config/sponsors.js
+++ src/config/sponsors.js
@@ -2,55 +2,55 @@ export default [
   {
     name: 'TEKsystems',
     desc: 'Whether it’s transforming your business, building an all-star team or finding the right fit for your professional ambitions, we’re in it with you. Let’s innovate and reinvent. Let’s own change, together.',
-    logo: 'fa-server',
+    logo: 'tek_systems.png',
     url: 'https://www.teksystems.com/'
   },
   {
     name: 'RMC Agency',
     desc: 'ARMC Michigan based, full-service Information Technology (IT) Consulting and Placement agency. The company offers highly experienced recruiting and account management professionals with the ability to service the diverse needs of Michigan companies seeking to employ top technical talent within the region - specializing in Applications, Infrastructure, and Communications expertise.',
-    logo: 'fa-apple',
+    logo: 'RMC.png',
     url: 'http://www.rmcagency.com/'
   },
   {
     name: 'SBDC Lansing',
     desc: 'The Michigan Small Business Development Center offers expert assistance at no cost to entrepreneurs looking to start or grow a business.',
-    logo: 'fa-wordpress',
+    logo: 'SBDC_MI.png',
     url: 'https://sbdcmichigan.org/'
   },
   {
     name: 'A2 Hosting',
     desc: 'We have a blazing fast web hosting solution perfect for you! Whether you have a low traffic, brand new blog or a popular business site, we\'ve got you covered! ',
-    logo: 'logo-line-graph',
+    logo: 'a2_hosting.png',
     url: 'https://www.a2hosting.com/'
   },
   {
     name: 'LEAP',
     desc: 'This is a group for UX design practitioners and those interested in learning about design.',
-    logo: 'fa-user-circle',
+    logo: 'leap.png',
     url: 'http://www.purelansing.com/'
   },
   {
     name: 'Humanity Codes',
     desc: '???',
-    logo: 'fa-moon-o',
+    logo: 'leap.png',
     url: 'https://www.lansing.codes/'
   },
   {
     name: 'BrightDigit',
     desc: 'The BrightDigit team brings over 15 years of experience in technology. Together they have a passion for mobile technology, public speaking, and education. Leo Dion, the founder of BrightDigit, is a sought-after speaker at professional groups about the benefits and capabilities of mobile technology.',
-    logo: 'fa-pie-chart',
+    logo: 'bright_digit.png',
     url: 'https://brightdigit.com/'
   },
   {
     name: 'TechSmith',
     desc: 'Founded in 1987, TechSmith pioneered the revolutionary idea of capturing screen content for better communication. Today, TechSmith is the world’s #1 source for visual communication software.',
-    logo: 'logo-mssql',
+    logo: 'tech_smith.png',
     url: 'https://www.techsmith.com/'
   },
   {
     name: 'Kunz, Leigh, and Associates',
     desc: 'Kunz, Leigh and Associates (KL&A) is a software development and professional services company currently serving the private and public sectors in Michigan and Ohio.',
-    logo: 'Cf',
+    logo: 'KLA.png',
     url: 'https://kunzleigh.com/'
   }
 ]
