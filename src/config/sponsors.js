1f51cadbc64b19390d4cfa1cae049c41d993c06a
Davis <d.crain@kunzleigh.com>
2018-10-09 07:51:26 -0400

    Add Humanity Codes desc, Edits to section title info

--- src/config/sponsors.js
+++ src/config/sponsors.js
@@ -31,7 +31,7 @@ export default [
   },
   {
     name: 'Humanity Codes',
-    desc: '???',
+    desc: 'Humanity Codes uses code and education to bring together Lansing\'s talented and diverse tech community.',
     logo: 'lansing-codes.png',
     url: 'https://www.lansing.codes/'
   },
