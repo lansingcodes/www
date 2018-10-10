fc5d7790d82d5d574e435acb6125f54f0a0bc437
P.F. Hawkins <correspondence@theoldevangelization.com>
2018-10-10 14:31:49 -0400

    Update Hyperverses' sponsor information.

--- src/config/sponsors.js
+++ src/config/sponsors.js
@@ -55,8 +55,8 @@ export default [
   },
   {
     name: 'Hyperverses',
-    desc: '???',
-    logo: 'lansing-codes.png',
+    desc: 'Hyperverses helps small and medium-sized businesses find the best buyers for their products and services with persuasive technology.'
+    logo: 'hyperverses.png',
     url: 'https://hyperverses.com/lansing-codes/'
   }
 ]
