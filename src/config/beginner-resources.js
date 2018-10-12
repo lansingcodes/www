021b1a1406b4fe664f960fdde36373e18b900347
Davis Crain <dhcrain@gmail.com>
2018-10-11 21:13:38 -0400

    replace Coders Farm with Code Lab 517
    Also a little more accessibility

--- src/config/beginner-resources.js
+++ src/config/beginner-resources.js
@@ -1,19 +1,22 @@
 export default [
   {
-    title: 'Coders Farm',
-    url: 'https://codersfarm.com/training/',
+    title: 'Code Lab 517',
+    url: 'https://www.codelab517.com/',
     desc: `
-      <p>If you're a hard worker, ready to change careers, apply to <a href="https://codersfarm.com/training/">Coders Farm</a>. Spots are very limited, but should you be accepted, you'll receive free training in web development <em>and</em> a part-time job until placed in a full-time position with another company.</p>
+      <p tabindex="0">Three weeks. Learn to code. Do you have an idea for a website or application, but don't know how to build it?
+      Are you a designer who wants to add development to your skillset?
+      Would you like to get more knowledgeable when talking to your company's developers?
+      </p>
     `
   },
   {
     title: 'Free Code Camp',
     url: 'https://www.freecodecamp.org',
     desc: `
-      <p>A community that works to help people learn to code and gain 
+      <p tabindex="0">A community that works to help people learn to code and gain 
       experience contributing to open source projects used by non-profits. 
       Learn to code by completing coding challenges and building projects. 
-      FreeCodeCamp is a donor supported non-profit and every aspect is 100% free.<p>
+      FreeCodeCamp is a donor supported non-profit and every aspect is 100% free.</p>
     `
   }
 ]
