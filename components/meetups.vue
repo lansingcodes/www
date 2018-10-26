8b5e3d4a597a6430cc85faf13a80d5199703475f
Katie Fritz <katie@katiemfritz.com>
2018-10-26 18:40:14 -0400

    add card to meetups page, add meetups config file

--- components/meetups.vue
+++ components/meetups.vue
@@ -1,5 +1,18 @@
 <template>
   <section>
     <h2>Free Meetups</h2>
+    <card
+      has-icon="true"
+      has-image="false"
+    />
   </section>
 </template>
+
+<script>
+import card from '~/components/card--small'
+export default {
+  components: {
+    card
+  }
+}
+</script>
