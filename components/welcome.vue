512e0bcaebf322a7f13616c29471d14fbb2e4c7f
Katie Fritz <katie@katiemfritz.com>
2018-10-25 21:16:49 -0400

    welcome component functional but not fully styled and still static

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,102 +1,46 @@
 <template>
-  <div
-    :style="headerStyles"
-    class="header">
-    <div class="header-content">
-      <div class="header-content-inner">
-        <h1 class="heading">
-          <slot name="heading">
-            Events and resources for Lansing coders
-          </slot>
-        </h1>
-        <hr class="divider">
-        <p>
-          <slot name="tagline">
-            for those who code or aspire to, professionally or as a hobby
-          </slot>
-        </p>
-        <slot name="call-to-action">
-          <button-link
-            type="primary"
-            icon="fa-calendar"
-            href="#calendar"
-          >
-            See what's happening now
-          </button-link>
-        </slot>
-      </div>
+  <div class="lc-background-image">
+    <div class="flex flex-wrap justify-center pt-8">
+      <section class="flex-none w-full sm:w-1/2 max-w-sm overflow-hidden shadow-lg bg-white text-center p-8 sm:mb-24">
+        <h2 class="uppercase text-blue text-3xl mb-4">Lansing Codes</h2>
+        <p>Events and resources for Lansing coders</p>
+        <hr class="border-4 border-blue my-6 w-1/6 ">
+        <p class="mb-4">for those who code or aspire to, professionally or as a hobby</p>
+        <img
+          class="h-32 w-32"
+          src="../assets/images/icon-tall-square-fixed-300-transparent.png"
+          alt="Lansing Codes Logo"
+        >
+      </section>
+
+      <section class="w-full sm:w-1/2 max-w-sm overflow-hidden shadow-lg bg-blue-dark text-white p-8 sm:mt-24 sm:-ml-4">
+        <h2 class="uppercase text-center text-3xl mb-4">Next Event</h2>
+        <hr class="border-4 border-white my-6 w-1/6 ">
+        <div class="text-left font-normal">
+          <h3 class="font-normal mb-2">Designing websites with Nuxt and Tailwind</h3>
+          <p class="text-xs mb-4">October 26, 2018, 7:00 PM</p>
+          <p class="mb-4">Sponsor: Lansing.Codes</p>
+          <p class="mb-6">
+            Annoy the old grumpy cat, start a fight and then retreat to wash when i lose
+            if human is on laptop sit on the keyboard for sit in a box for hours for eat the
+            rubberband. Curl into a furry donut ignore the human until she needs to get up,
+            then climb on her lap and sprawl.
+          </p>
+        </div>
+        <div>
+          <button class="bg-white hover:bg-grey-lightest text-blue font-bold uppercase text-center py-4 px-8 min-w-24 rounded-full">
+            Show More
+          </button>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
-<script>
-import buttonLink from '~components/button-link'
-
-export default {
-  components: {
-    buttonLink
-  },
-  props: {
-    backgroundImage: {
-      type: String,
-      default: null
-    }
-  },
-  computed: {
-    headerStyles() {
-      const styles = {}
-      if (this.backgroundImage) {
-        styles.backgroundImage = `url(${this.backgroundImage})`
-      }
-      return styles
-    }
-  }
-}
-</script>
-
-<style lang="scss" scoped>
-.header {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  min-height: 100vh;
-  text-align: center;
-  color: #fff;
-  //background-image: url('/static/images/capitol.jpg');
-  background-position: top;
-  background-size: cover;
-}
-
-.header-content {
-  width: 100%;
-  height: 100%;
-  background-color: rgba(255, 255, 255, 0.75);
-
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-}
-
-.header-content-inner {
-  color: #407cbf;
-  box-shadow: none;
-  background: transparent;
-  p {
-    color: #000 !important;
-  }
-}
-
-@media (max-width: 767px) {
-  header {
-    min-height: auto;
-  }
-
-  .header-content {
-    padding: 100px 15px;
-    position: relative;
-    top: auto;
-    transform: none;
-  }
+<style scoped>
+.lc-background-image {
+  background-position: center top;
+  background-repeat: no-repeat;
+  background-image: url('../assets/images/capitol.jpg');
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <section class="container">
-    <h1>Hello world</h1>
-    <font-awesome-icon :icon="['fas', 'code']" />
-    <font-awesome-icon :icon="['fas', 'hourglass-half']" />
-    <font-awesome-icon :icon="['fab', 'twitter']" />
-  </section>
+  <div>
+    <welcome/>
+  </div>
 </template>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS */
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+<script>
+import welcome from '~/components/welcome'
+
+export default {
+  components: {
+    welcome
+  }
 }
-</style>
+</script>
