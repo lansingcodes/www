ffef007b6551c1e9bc27167990bf4ab8a5bb97aa
Katie Fritz <katie@katiemfritz.com>
2018-12-13 15:25:45 -0500

    change local settings; try changing event key to be id instead of name but it's not working yet

--- components/events.vue
+++ components/events.vue
@@ -12,15 +12,14 @@
 
     <eventCard
       v-for="event in events"
-      :key="event.name"
+      :key="event.attributes.id"
       :event-name="event.attributes.name"
       :event-link="event.links.self"
       :event-date="formatDate(event.attributes.time.absolute)"
       :event-description="event.attributes.description"
       :venue="event.relationships.venue.attributes"
-      :expanded-event-name="expandedEventName"
-      class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4
-        mb-8 mx-auto"
+      :expanded-event-id="expandedEventId"
+      class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-8 mx-auto"
       @card-event-expand="expandEventCard"
       @card-event-collapse="collapseEventCard"
     />
@@ -40,7 +39,7 @@ export default {
   },
   data() {
     return {
-      expandedEventName: null
+      expandedEventId: null
     }
   },
   computed: {
@@ -52,12 +51,12 @@ export default {
     formatDate(date) {
       return format(date, 'dddd, MMMM D [at] h:mm a')
     },
-    expandEventCard(emittedEventName) {
-      this.expandedEventName = emittedEventName
+    expandEventCard(emittedEventId) {
+      this.expandedEventId = emittedEventId
     },
-    collapseEventCard(emittedEventName) {
-      if (this.expandedEventName === emittedEventName) {
-        this.expandedEventName = null
+    collapseEventCard(emittedEventId) {
+      if (this.expandedEventId === emittedEventId) {
+        this.expandedEventId = null
       }
     }
   }
