28ec43e01fc1f539afdaec5a07e99d607b90e9e0
Erik Gillespie <erik.gillespie@gmail.com>
2019-06-06 19:49:01 -0400

    Use top and bottom SVGs on resources section to scale disproportionately

--- components/resources.vue
+++ components/resources.vue
@@ -1,38 +1,49 @@
 <template>
-  <section
-    id="resources"
-    class="
-      flex flex-wrap justify-center w-full bg-blue py-16 px-4 mb-16
-      md:py-32 md:mb-32
-    "
-  >
-    <div class="w-full">
-      <section-heading
-        white
-        heading="Beginner Resources"
-        subheading="where to start if you're new to coding"
-        class="w-full md:w-1/2 md:ml-auto md:mr-0 md:-mt-16 md:mb-4 md:pl-8"
-      />
-    </div>
+  <section id="resources">
     <div
-      v-for="resource in resources"
-      :key="resource.title"
-      class="font-serif w-full max-w-sm px-4 py-8 md:w-1/2 md:px-3 md:mb-20"
+      class="
+        lc-bg-rtl-gradient sm:lc-bg-upright-trapezoid bg-white h-40
+      "
     >
-      <h3 class="text-2xl font-sans font-normal text-blue mb-2">
-        <a
-          :href="resource.url"
-          target="_blank"
-          rel="noreferrer noopener"
-          class="no-underline text-white hover:underline hover:text-white"
-        >
-          {{ resource.title }}
-        </a>
-      </h3>
-      <div class="text-white">
-        {{ resource.desc }}
+      <div
+        class="container mx-auto flex sm:justify-end flex-no-wrap content-center"
+      >
+        <section-heading
+          white
+          heading="Beginner Resources"
+          subheading="where to start if you're new to coding"
+          class="w-full sm:w-2/5 mb-0 mt-10 mx-auto sm:mx-0"
+        />
       </div>
     </div>
+    <div class="lc-bg-rtl-gradient">
+      <div
+        class="
+          container mx-auto flex flex-wrap content-center justify-around py-16
+        "
+      >
+        <div
+          v-for="resource in resources"
+          :key="resource.title"
+          class="font-serif w-full max-w-sm px-4 my-6 md:w-1/2"
+        >
+          <h3 class="text-2xl font-sans font-normal text-blue mb-2">
+            <a
+              :href="resource.url"
+              target="_blank"
+              rel="noreferrer noopener"
+              class="no-underline text-white hover:underline hover:text-white"
+            >
+              {{ resource.title }}
+            </a>
+          </h3>
+          <div class="text-white">
+            {{ resource.desc }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden sm:block lc-bg-downright-trapezoid bg-white h-32"/>
   </section>
 </template>
 
@@ -53,15 +64,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-  background-image: linear-gradient(to top right, #005198, #2979bf);
+.lc-bg-rtl-gradient {
+  background: linear-gradient(to left, #2979bf, #005198);
+}
 
-  @media (min-width: 768px) {
-    background-color: transparent;
-    background-image: url(../assets/images/bg/resources.svg);
-    background-position: center top;
+.lc-bg-downright-trapezoid {
+  background-color: #fff;
+  background-image: url(../assets/images/bg/downright-trapezoid.svg);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+@media (min-width: 576px) {
+  .sm\:lc-bg-upright-trapezoid {
+    background-color: #fff;
+    background-image: url(../assets/images/bg/upright-trapezoid.svg);
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: 100% 100%;
   }
 }
 </style>
