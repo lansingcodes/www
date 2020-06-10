be9240cb584ec5770242bf1f451adb960084a3a3
Erik Gillespie <erik.gillespie@gmail.com>
2020-06-09 20:04:33 -0400

    Add space between event list and page edges

--- components/event-list.vue
+++ components/event-list.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    class="
-      flex flex-wrap align-start justify-around
-    "
-  >
+  <div class="flex flex-wrap align-start justify-around px-4">
     <event-card
       v-for="event in events"
       :key="event.id"
