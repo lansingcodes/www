b429e2547b00560c60b937d11b5e8c7a0e394a2f
Walter <eschenbach.walter@gmail.com>
2021-10-09 23:17:35 -0500

    Refactor clean-event-description utility function by implementing regex

--- utils/clean-event-description.js
+++ utils/clean-event-description.js
@@ -1,52 +1,22 @@
 export default description => {
   // Trim whitespace
-  let nDescription = description.trim()
+  let newDescription = description.trim()
   // Remove elipsis at end of description
-  nDescription =
-    nDescription
-      .split('')
-      .reverse()
-      .splice(0, 3)
-      .join('') === '...'
-      ? description
-          .split('')
-          .reverse()
-          .splice(3, description.length)
-          .reverse()
-          .join('')
-      : description
+  newDescription = newDescription.replace(/\.\.\./g, '')
   // Remove emoji from description
-  nDescription.replace(
+  //https://stackoverflow.com/questions/10992921/how-to-remove-emoji-code-using-javascript
+  newDescription = newDescription.replace(
     /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF])/g,
     ''
   )
   // If the description contains any '.', remove all text after the last '.'
-  const lastPeriodIndex = nDescription
-    .split('')
-    .reverse()
-    .join('')
-    .indexOf('.')
-  nDescription =
-    lastPeriodIndex !== -1
-      ? nDescription
-          .split('')
-          .splice(0, nDescription.length - lastPeriodIndex)
-          .join('')
-      : nDescription
+  newDescription = newDescription.replace(/\.(?=[^.]*$)(.*)/g, '')
   // If the description contains ---, remove the --- and all subsequent text, then trim the text again
-  nDescription =
-    nDescription.indexOf('---') !== -1
-      ? nDescription
-          .split('')
-          .splice(0, nDescription.indexOf('---'))
-          .join('')
-      : nDescription
+  newDescription = newDescription.replace(/(---)(.*)/g, '')
   // Trim whitespace
-  nDescription = nDescription.trim()
+  newDescription = newDescription.trim()
   // Re-add the trailing ... characters
-  nDescription =
-    nDescription[nDescription.length - 1] !== '.'
-      ? `${nDescription}...`
-      : nDescription
-  return nDescription
+  newDescription = newDescription.concat('...')
+
+  return newDescription
 }
