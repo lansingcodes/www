de43448113288eb811b98298802aa1059ea856fd
Zowie Beha <113861530+zowiebeha@users.noreply.github.com>
2025-10-19 13:50:36 -0400

    Handle lone opening double-asterisk
    In certain cases an ending markdown bold tag
    may be truncated or omitted from the author's event
    description. This commit handles these cases gracefully.
    
    Also adds a space of formatting missing from a comment
    in clean-event-description.js

--- utils/bold-markdown-to-html.js
+++ utils/bold-markdown-to-html.js
@@ -1,16 +1,33 @@
 /*
- * Replace pairs of "**" with opening and closing span tags
- * ... to give text between the pairs a bold style.
+ * Replace '**' markdown tags with <span> HTML markup tags
+ * .. to give text a bold style.
+ * E.g '**text** **welcome' becomes:
+ * .. '<span class="font-bold">text</span> <span class="font-bold">welcome</span>'
+ * 
  * @param {string} markdown - input markdown string
  * @returns {string}
  */
 export default markdown => {
+  const HTMLOpeningSpan = '<span class="font-bold">'
+  const HTMLEndingSpan = '</span>'
   const markdownMatches = [...markdown.matchAll(/\*\*/g)]
   while (markdownMatches[0] && markdownMatches[1]) {
-    markdown = markdown.replace('**', '<span class="font-bold">')
-    markdown = markdown.replace('**', '</span>')
+    markdown = markdown.replace('**', HTMLOpeningSpan)
+    markdown = markdown.replace('**', HTMLEndingSpan)
     markdownMatches.shift()
     markdownMatches.shift()
+  }
+  // If an ending markdown tag was lost during truncation
+  // .. within clean-event-description (or forgotten when authoring the description),
+  // .. ensure text that follows the opening double-asterisk is bold.
+  if (markdownMatches[0]) {
+    // If there is no text to make bold after the opening '**'
+    if (markdown.endsWith('**')) {
+      markdown = markdown.replace('**', '')
+    } else {
+      markdown = markdown.replace('**', HTMLOpeningSpan)
+      markdown = markdown + HTMLEndingSpan
+    }
   }
   return markdown
 }
