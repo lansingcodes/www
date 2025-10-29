0d3d8bd06d465c3b656fd4fe06e4df3e17aaad8d
Adam Broadbent <abroadbent@live.com>
2025-10-28 23:00:56 -0400

    Remove unused server chunks and related files to optimize build size and performance

--- .output/server/chunks/virtual/_virtual_spa-template.mjs
+++
@@ -1,4 +0,0 @@
-globalThis.__timing__.logStart('Load chunks/virtual/_virtual_spa-template');const template = "";
-
-export { template };;globalThis.__timing__.logEnd('Load chunks/virtual/_virtual_spa-template');
-//# sourceMappingURL=_virtual_spa-template.mjs.map
